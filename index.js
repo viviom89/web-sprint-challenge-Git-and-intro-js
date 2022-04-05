bc2333b829638ac9e61e5fa246a6262b61f0be9c
Chhorvy Om <vivi@chhorvys-imac.attlocal.net>
2022-04-04 19:42:38 -0700

    Added if method in Task 8 line 341; moved return method in Task 9 out of for scope

--- index.js
+++ index.js
@@ -336,12 +336,14 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(array, name){
-  for (let i = 11; i <= artists.length-1; i++) {
-    return artists[i].bio;
+function artistInfo(artists, name){
+  for (let i = 0; i <= artists.length-1; i++) {
+    if (artists[i].name === name) {
+      return artists[i].bio;
+    }
   }
-}
-//console.log(artistInfo(artists, 'Kazimir Malevich'));
+};
+//console.log(artistInfo(artists, 'Frida Kahlo'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
